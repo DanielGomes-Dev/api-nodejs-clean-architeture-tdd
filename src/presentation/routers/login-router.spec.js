b49a503ec3125c3d436baad80763abd1ee852f3d
Daniel Gomes <danielgds@id.uf.br>
2021-02-16 15:21:00 -0300

    ensure LoginRouter returns 400 if no password is provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,6 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email) return { statusCode: 400 }
+    if (!httpRequest.body.email || !httpRequest.body.password) return { statusCode: 400 }
   }
 }
 
@@ -12,6 +12,20 @@ describe('Login Router', () => {
       body: {
         // email:'valid_email@mail.com',
         password: 'valid_password'
+      }
+    }
+
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 400 if no password is provided', () => {
+    // sut System un Test
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'valid_email@mail.com'
+        // password: 'valid_password'
       }
     }
 
