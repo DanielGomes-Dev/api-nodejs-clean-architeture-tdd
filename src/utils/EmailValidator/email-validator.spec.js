7cd1235aa61c2ec0e2cc61c044c1f4cd3316f110
Daniel Gomes <danielgds@id.uf.br>
2021-02-20 12:42:07 -0300

    move sut creation to a factory method

--- src/utils/EmailValidator/email-validator.spec.js
+++ src/utils/EmailValidator/email-validator.spec.js
@@ -6,16 +6,21 @@ class EmailValidator {
   }
 }
 
+const makeSut = () => {
+  // Criar um makeSut para caso ela precise de alguma dependencia não sera nescessario alterar todos os testes;
+  return new EmailValidator()
+}
+
 describe('Email Validator', () => {
   test('Should return true if validator return true', () => {
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailvalid = sut.isValid('valid_email@mail.com')
     expect(isEmailvalid).toBe(true)
   })
 
   test('Should return false if validator return false', () => {
     validator.isEmailValid = false
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('invalid_email@mail.com')
 
     expect(isEmailValid).toBe(false)
