d3f4a1daa00175325bae872c582b2c3cbbbb2a4c
Daniel Gomes <danielgomes1@live.com>
2021-03-09 17:37:36 -0300

    ensure Encrypter calls bcrypt with correct values

--- src/utils/Encrypter/encrypter.spec.js
+++ src/utils/Encrypter/encrypter.spec.js
@@ -16,7 +16,16 @@ describe('Ecrypter', () => {
     });
     test('Should return false if bcrypt returns false', async () => {
         const sut = new Encrypter()
+        bcrypt.isValid = false;
         const isValid = await sut.compare('any_value','hashed_value')
         expect(isValid).toBe(false)
     });
+    test('Should call bcrypt with correct values', async () => {
+        const sut = new Encrypter()
+        await sut.compare('any_value','hashed_value')
+        expect(bcrypt.value).toBe('any_value')
+        expect(bcrypt.hash).toBe('hashed_value')
+        
+    });
+
 });
