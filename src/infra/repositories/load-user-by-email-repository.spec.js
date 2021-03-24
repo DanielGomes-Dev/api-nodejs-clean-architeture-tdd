991ecb68831888432be43a36563a4dadd1d6b655
Daniel Gomes <danielgomes1@live.com>
2021-03-24 11:46:11 -0300

    ensure LoadUserByEmailRepository throws if no email is provided

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -1,5 +1,6 @@
 const MongoHelper = require('../helpers/mongo-helper');
 const  LoadUserByEmailRepository  = require('./load-user-by-email-repository')
+const MissingParamError = require('../../utils/errors/missing-param-error');
 let db
 
 const makeSut = () =>{
@@ -43,10 +44,24 @@ describe('LoadUserByEmail', () => {
             state: 'any_name',
             password: 'hashed_password'
         });
+
         const user = await sut.load('valid_email@mail.com')
         expect(user).toEqual({
             _id:fakeUser.ops[0]._id,
             password:fakeUser.ops[0].password
         });
     });
+
+    test('Should throw if no userModel is provided', async () => {
+      const sut = new LoadUserByEmailRepository();
+      const promise = sut.load('any_email@mail.com')
+      expect(promise).rejects.toThrow();
+    });
+
+    test('Should throw if no userModel is provided', async () => {
+      const {sut} = makeSut();
+      const promise = sut.load()
+      expect(promise).rejects.toThrow(new MissingParamError('email'));
+    });
+
 });
