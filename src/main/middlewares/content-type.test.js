3733d973fed23da1f1fa7c46763fb478e19070f6
Daniel Gomes <danielgomes1@live.com>
2023-01-10 06:23:40 -0300

    reset jest modules in content type test middlewares

--- src/main/middlewares/content-type.test.js
+++ src/main/middlewares/content-type.test.js
@@ -1,7 +1,14 @@
 const request = require('supertest')
-const app = require('../config/app')
+    // const app = require('../config/app')
 
 describe('content-type middleware', () => {
+    let app;
+
+    beforeEach(() => {
+        jest.resetModules()
+        app = require('../config/app')
+
+    })
 
     test('should return json content type as default', async() => {
         app.get('/content-type', (req, res) => {
@@ -10,6 +17,28 @@ describe('content-type middleware', () => {
 
         const res = await request(app).get('/content-type')
             .expect('content-type', /json/)
+
+    });
+
+    test('should return xml content type af forced', async() => {
+        app.get('/content-type', (req, res) => {
+            res.type('xml')
+            res.send({})
+        })
+
+        const res = await request(app).get('/content-type')
+            .expect('content-type', /xml/)
+
+    });
+
+    test('should return xml content type af forced', async() => {
+        app.get('/content-type-xml', (req, res) => {
+            res.type('xml')
+            res.send({})
+        })
+
+        const res = await request(app).get('/content-type-xml')
+            .expect('content-type', /xml/)
 
     });
 });
